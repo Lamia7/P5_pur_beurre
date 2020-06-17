0dff6fcb45a037e29e842510304cb33f63c87704
Lamia EL RALIMI <lamia.elralimi@nova-technology.fr>
2020-06-17 15:20:35 +0200

    ✨ Added 'category' and 'store' modules

---
+++ models/category.py
@@ -0,0 +1,11 @@
+"""Module that manages Category table"""
+
+
+class Category:
+
+    def __init__(self, id, name):
+        self.id = id
+        self.name = name
+
+    def __str__(self):
+        pass
---
+++ models/store.py
@@ -0,0 +1,11 @@
+"""Module that manages Store table"""
+
+
+class Store:
+
+    def __init__(self, id, name):
+        self.id = id
+        self.name = name
+
+    def __str__(self):
+        pass

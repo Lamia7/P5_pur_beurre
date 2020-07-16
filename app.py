adf85a82751ad06c07b5190956462b4a83f36784
Lamia EL RALIMI <lamia.elralimi@nova-technology.fr>
2020-07-16 11:03:48 +0200

    🐛 Fixed bug: delete duplicate product_store

--- app.py
+++ app.py
@@ -37,16 +37,19 @@ print("Categories inserted to database.")
 # ---------- Insert product_id and category_id to product_category table ---------- #
 # Creates a list of ProductCategory objects (ids)
 product_category_obj_list = []
-product_store_obj_list = []
 for product in downloaded_products:
     product_id = product.id  # id de l'objet product de la list
     for category in product.categories:
         category_id = category.id
         product_category_obj_list.append(ProductCategory(product_id, category_id))
-        for store in product.stores:
-            store_id = store.id
-            product_store_obj_list.append(ProductStore(product_id, store_id))
-        #product_category_obj_list.append(ProductCategory(product_id, category_id))
+
+# Creates a list of ProductStore objects (ids)
+product_store_obj_list = []
+for product in downloaded_products:
+    product_id = product.id  # id de l'objet product de la list
+    for store in product.stores:
+        store_id = store.id
+        product_store_obj_list.append(ProductStore(product_id, store_id))
 
 pcm = ProductCategoryManager()
 psm = ProductStoreManager()
@@ -55,8 +58,3 @@ print("Product_Category inserted to database.")
 psm.insert_product_and_store_ids(product_store_obj_list)
 print("Product_Store inserted to database.")
 
-"""# ---------- Insert each store to database ---------- #
-sm = StoreManager()
-for one_product in downloaded_products:
-    sm.insert_store(one_product)
-print("Stores inserted to database.")"""
